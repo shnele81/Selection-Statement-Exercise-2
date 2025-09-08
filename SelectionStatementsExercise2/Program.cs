44e16c7f1f29c71c4993b47b90231763e4c1be1c
Andrew ONeill <andy@evolve757.com>
2025-09-08 09:17:31 -0400

    finsiehd 2nd assignment

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("That's great! Math is great for solving problems.");
+                    break;
+                case "science":
+                    Console.WriteLine("That's great! Science is great for learning about how the world works.");
+                    break;
+                case "history":
+                    Console.WriteLine("That's great! History helps us understand where we came from.");
+                    break;
+                case "english":
+                    Console.WriteLine("That's great! English teaches us how to read and write.");
+                    break;
+                case "pe":
+                    Console.WriteLine("That's great! Exercise and physical health is important.");
+                    break;
+                default:
+                    Console.WriteLine("That's an interesting choice. What is your favorite part about that subject?");
+                    break;
+                
+            }
         }
     }
 }
